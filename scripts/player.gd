0212a0f80308597895889107dc80d2a655f3d8e7
Aaron <sergeantbeast456@gmail.com>
2024-07-24 13:25:29 -0400

    Added functionality for the player to toggle to the shadow and control the shadow independently

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,24 +1,48 @@
 extends CharacterBody2D
 
+# Controls player movement throughout the game
 @export var speed := 20000
 @export var sprint_speed := 30000
 @export var jump_speed := -550
 
+# Signal for indicating the player is changing to the shadow
+signal controls_toggle
+
+# Effects the speed at which the player falls
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+# Flips when the player presses tab
+var controlled := true
+
+# Handles player signals
+func _process(_delta):
+	# Emits a message to the world that the player is toggling to the shadow
+	if (Input.is_action_just_pressed("ToggleShadow")):
+		controls_toggle.emit()
+
+# Handles player controlls
 func _physics_process(delta):
 	# Adding the gravity
 	velocity.y += gravity * delta
 	
-	# Handle Jump
-	if Input.is_action_just_pressed("Jump") && is_on_floor():
-		velocity.y += jump_speed
-	
-	# Get the input direction
-	var direction = Input.get_axis("StrafeLeft", "StrafeRight")
-	if Input.is_action_pressed("Sprint"):
-		velocity.x = direction * sprint_speed * delta
-	else:
-		velocity.x = direction * speed * delta
+	if controlled:
+		# Handle Jump
+		if Input.is_action_just_pressed("Jump") && is_on_floor():
+			velocity.y += jump_speed
+			
+		# Get the input direction
+		var direction = Input.get_axis("StrafeLeft", "StrafeRight")
+		if Input.is_action_pressed("Sprint"):
+			velocity.x = direction * sprint_speed * delta
+		else:
+			velocity.x = direction * speed * delta
 	
 	move_and_slide()
+
+# Toggles the player to no longer have control until activated again
+func controls_toggled():
+	if controlled:
+		controlled = false
+		velocity.x = 0.0
+	else:
+		controlled = true
--- scripts/playercamera.gd
+++ scripts/playercamera.gd
@@ -1,11 +1,24 @@
 extends Camera2D
 
 @export var player : CharacterBody2D = null
+@export var shadow : Node2D = null
+
+var follow_player := true
 
 func _ready():
-	if player != null:
+	if player != null && follow_player:
 		position = player.position
+	elif shadow != null:
+		position = shadow.position
 
 func _process(_delta):
-	if player != null:
+	if player != null && follow_player:
 		position = player.position
+	elif shadow != null:
+		position = shadow.position
+
+func controls_toggled():
+	if follow_player:
+		follow_player = false
+	else:
+		follow_player = true
