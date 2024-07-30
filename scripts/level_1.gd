f6ce7e75f625fc76a79187715bc4ef764cd9d6ee
Aaron <sergeantbeast456@gmail.com>
2024-07-30 18:49:45 -0400

    fix: player can toggle to shadow again

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -3,9 +3,10 @@ extends Node2D
 @onready var pause_menu = $Player/PauseMenu
 var paused = false
 
+func _on_player_controls_toggle():
+	get_tree().call_group("player", "controls_toggled")
 
-
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_just_pressed("Pause"):
 		pauseMenu()
 		
